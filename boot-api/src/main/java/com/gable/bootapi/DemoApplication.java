232ed356704cb36919ba2cc086c767130e0934d8
boy <prasert.mx@gmail.com>
2019-08-29 16:37:31 +0700

    init

---
+++ boot-api/src/main/java/com/gable/bootapi/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.gable.bootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
